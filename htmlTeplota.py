901042c23e33ded4a28f5bd6d8d5d735b78052ff
Adam Votoček <popvotocek@gmail.com>
2018-05-20 19:03:46 +0200

    pridan ajax do htmlTeplota.py

--- htmlTeplota.py
+++ htmlTeplota.py
@@ -4,6 +4,22 @@ import SocketServer
 
 sense = SenseHat()
 
+temp = 0
+
+def getHtml(temp):
+    return (("""<html><head><script""" +
+""" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>""" +
+"""</head><body><h1>Teplota: <span id="temp">%s</span>C</h1>""" + 
+"""<script>""" +
+"""$(document).ready(function(){""" +
+"""   setInterval(function(){""" +
+"""      $.get("get-temp", function (data){""" +
+"""         $("#temp").html(data)""" +
+"""      });""" +
+"""   },2000);"""
+"""});""" +
+"""</script></body></html>""") % round(temp, 2))
+
 class MyHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def _set_headers(self):
         self.send_response(200)
@@ -13,10 +29,14 @@ class MyHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_GET(self):
         temp = sense.get_temperature()
         self._set_headers()
-        self.wfile.write("<html><body><h1>Teplota: %s C</h1></body></html>" % temp)
+        if self.path == "/get-temp":
+            print("QWERTZ")
+            self.wfile.write("%s" % round(temp, 2))
+        else:
+            self.wfile.write(getHtml(temp))
+        print("00000" + self.path + "00000")
 
-
-PORT = 8000
+PORT = 8001
 
 Handler = MyHandler #SimpleHTTPServer.SimpleHTTPRequestHandler
 
